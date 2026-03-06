c1eea130bc49bb16828147999f8f3ed69eb3b413
lexyblazy <adekola.olalekan01@gmail.com>
2026-03-06 15:46:46 -0600

    cleanup server.go

--- backend/cmd/server/server.go
+++ backend/cmd/server/server.go
@@ -1,46 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
 	"github.com/lexyblazy/gowords/internal/config"
-
-	"github.com/gorilla/websocket"
 	"github.com/lexyblazy/gowords/internal/lobby"
+	"github.com/lexyblazy/gowords/internal/server"
+	"github.com/lexyblazy/gowords/internal/store"
 )
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
 
 func main() {
 	c := config.New("config.json")
 	l := lobby.New(c)
 	l.Init()
+	db := store.NewSqlDB(c.Db.FileName)
 
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		player := lobby.NewPlayer(conn, nil, "", "", l)
-		go player.ReadPump()
-		player.WritePump()
-
-	})
-
-	log.Println("Server started on port", c.Server.Port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", c.Server.Port), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	s := server.New(db, c.Server.Port, l)
+	s.Start()
 
 }
