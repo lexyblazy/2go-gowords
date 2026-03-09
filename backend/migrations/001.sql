b1118cca926e98a3b8cf5767cbde04d8c60f9d73
lexyblazy <adekola.olalekan01@gmail.com>
2026-03-09 17:25:42 -0500

    update migration file

--- backend/migrations/001.sql
+++ backend/migrations/001.sql
@@ -1,13 +1,19 @@
--- up
 CREATE TABLE users (
     id uuid PRIMARY KEY NOT NULL, 
     username VARCHAR(255) UNIQUE NOT NULL,
-    moniker VARCHAR(255) DEFAULT "username", -- for fancy customization purposes. 
+    moniker VARCHAR(255) NOT NULL, -- for fancy customization purposes. 
     password VARCHAR(255) NOT NULL,
     recovery_hash TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 
--- down
-DROP TABLE users
+CREATE TABLE user_stats (
+  user_id uuid PRIMARY KEY NOT NULL,
+  games_played INTEGER DEFAULT 0,
+  wins_count INTEGER DEFAULT 0,
+  best_score INTEGER DEFAULT 0,
+  total_score INTEGER DEFAULT 0,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
